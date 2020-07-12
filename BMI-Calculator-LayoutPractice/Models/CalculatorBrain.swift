f9bf23ee589294eb384158f586036dfaeb58a8fa
Mahmoud Shabana <mshabana5656@gmail.com>
2020-07-11 20:01:32 -0400

    font fixes

--- BMI-Calculator-LayoutPractice/Models/CalculatorBrain.swift
+++ BMI-Calculator-LayoutPractice/Models/CalculatorBrain.swift
@@ -19,11 +19,11 @@ struct CalculatorBrain {
         let bmiValue =  703 * (weight / pow(height, 2))
         
         if bmiValue <= 18.5 {
-            bmi = BMI(value: bmiValue, advice: "Eat more Pizza", color: #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1))
+            bmi = BMI(value: bmiValue, advice: "Eat MORE Pizza", color: #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1))
         } else if bmiValue <= 24.9 {
             bmi = BMI(value: bmiValue, advice: "You're in good shape!", color: #colorLiteral(red: 0.5843137503, green: 0.8235294223, blue: 0.4196078479, alpha: 1))
         } else {
-            bmi = BMI(value: bmiValue, advice: "Eat less Pizza", color: #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1))
+            bmi = BMI(value: bmiValue, advice: "Eat LESS Pizza", color: #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1))
         }
     }
     
